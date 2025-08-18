f5ea1c76a4431a083015c873ec7a0f588da3a706
Thomas Refis <thomas.refis@gmail.com>
2025-08-18 17:41:03 +0400

    test: support for focused fuzzer output

--- test/gen-dune.sh
+++ test/gen-dune.sh
@@ -1,8 +1,7 @@
-for f in $@; do
-    case $f in
-        *.ml)
-            name=$(basename -s ".ml" $f)
-            cat <<EOF
+function implementation_file {
+    local f="$1"
+    name=$(basename -s ".ml" $f)
+    cat <<EOF
 (rule
   (with-stdout-to nocomment-$f
     (run ../tools/strip_comments.exe %{dep:$f})))
@@ -24,7 +23,61 @@ for f in $@; do
   (deps (alias $name)))
 
 ;; ---
+
 EOF
+}
+
+function fuzzer_output {
+    BASE=$(basename -s '.mls' $1)
+
+    for j in $(seq 1 $(wc -l $1 | cut -d' ' -f1)); do
+        (( i = j - 1 ))
+        f="$BASE-$i"
+        FILENAMES[$i]="$f"
+
+        cat <<EOF
+(rule
+  (with-stdout-to printed-$f
+    (run ../bin/stylo.exe %{dep:$f})))
+
+(rule
+  (alias $BASE)
+  (action (diff %{dep:$f} %{dep:printed-$f})))
+
+EOF
+    done
+
+    cat <<EOF
+(rule
+  (write-file $BASE.sh
+        "#!/bin/bash
+
+(( j = 0 ))
+while read line; do
+    echo \$line > $BASE-\$j
+    (( j++ ))
+done"))
+
+(rule
+    (targets ${FILENAMES[@]})
+    (action
+      (progn
+        (bash "chmod +x %{dep:./$BASE.sh}")
+        (with-stdin-from %{dep:$1}
+          (run %{dep:./$BASE.sh})))))
+;; ---
+
+EOF
+}
+
+for f in $@; do
+    case $f in
+        *.ml)
+            implementation_file "$f"
+            ;;
+
+        *.mls)
+            fuzzer_output "$f"
             ;;
 
         *)
