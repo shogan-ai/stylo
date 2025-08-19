3c678d35b2d356a550a86643c5f608151dc55af8
Thomas Refis <thomas.refis@gmail.com>
2025-08-19 10:39:30 +0400

    test: less ws noise

--- bin/stylo.ml
+++ bin/stylo.ml
@@ -3,4 +3,6 @@ let () =
   let lexbuf = Lexing.from_channel ic in
   let ast = Parse.(Parser.implementation Lexer.token) lexbuf in
   let doc = Print.Structure.pp_implementation ast in
-  PPrint.ToChannel.pretty 1. 80 stdout doc
+  PPrint.ToChannel.pretty 1. 80 stdout (PPrint.group doc);
+  print_newline ();
+  flush stdout
