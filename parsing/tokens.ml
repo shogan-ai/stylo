314f0b77c29885947f5fc905e497cd2a9b799767
Thomas Refis <thomas.refis@gmail.com>
2025-09-11 11:56:34 +0200

    token tree: handle empty reductions

--- parsing/tokens.ml
+++ parsing/tokens.ml
@@ -1,93 +1,6 @@
 
 type token = Parser_tokens.token
 
-type desc =
-  | Token of token
-  | Comment of string
-  | Child_node
-
-type elt = {
-  desc: desc;
-  pos: Lexing.position;
-}
-
-type seq = elt list
-
-module Indexed_list : sig
-  type t
-
-  val create : unit -> t
-  val append : t -> pos:Lexing.position -> desc -> unit
-  val consume : t -> Lexing.position -> Lexing.position -> seq
-
-  val global : t
-  val reset_global : unit -> unit
-end = struct
-  open Lexing
-
-  type cell =
-    | Empty
-    | Node of {
-        pos: position;
-        mutable value: elt;
-        mutable next: cell;
-      }
-
-  module Tbl =
-    Hashtbl.Make(struct
-      type t = position
-      let equal t1 t2 = Int.equal t1.pos_cnum t2.pos_cnum
-      let hash t = Hashtbl.hash t.pos_cnum
-    end)
-
-  type t = {
-    tbl: cell Tbl.t;
-    mutable last: cell;
-  }
-  let create () : t = { tbl = Tbl.create 42; last = Empty }
-
-  let append t ~pos desc =
-    let elt = { desc; pos } in
-    let node = Node { pos; value = elt; next = Empty } in
-    Tbl.add t.tbl pos node;
-    begin match t.last with
-    | Empty -> ()
-    | Node n -> n.next <- node
-    end;
-    t.last <- node
-
-  let consume t start stop =
-    let rec aux = function
-      | Empty -> invalid_arg "Tokens.consume"
-      | Node n as curr ->
-        if n.pos >= stop then (
-          (* Stop is an endpos, and we index by startpos *)
-          [], curr
-        ) else (
-          Tbl.remove t.tbl n.pos;
-          let seq, after = aux n.next in
-          n.value :: seq, after
-        )
-    in
-    match Tbl.find t.tbl start with
-    | Empty -> invalid_arg "Tokens.consume"
-    | Node start_node as cell->
-      let seq, after = aux cell in
-      start_node.value <- { start_node.value with desc = Child_node };
-      start_node.next <- after;
-      Tbl.replace t.tbl start cell; (* has been removed by aux... *)
-      Tbl.replace t.tbl stop cell;
-      seq
-
-  let global = create ()
-  let reset_global () =
-    Tbl.clear global.tbl;
-    global.last <- Empty
-end
-
-let at (startpos,endpos) =
-  Indexed_list.(consume global startpos endpos)
-
 module Raw = struct
   let to_string = function
   | Parser_tokens.AMPERAMPER  -> "AMPERAMPER"
@@ -381,6 +294,18 @@ module Raw = struct
   | _ -> false
 end
 
+type desc =
+  | Token of token
+  | Comment of string
+  | Child_node
+
+type elt = {
+  desc: desc;
+  pos: Lexing.position;
+}
+
+type seq = elt list
+
 let desc_as_string = function
   | Token t ->
     if Dbg_print.dbg then
@@ -397,3 +322,134 @@ let pp_seq =
   pp_print_list
     ~pp_sep:(fun ppf () -> fprintf ppf ",@ ")
     pp_elt
+module Indexed_list : sig
+  type t
+
+  val create : unit -> t
+  val append : t -> pos:Lexing.position -> desc -> unit
+  val consume : t -> Lexing.position -> Lexing.position -> seq
+
+  val global : t
+  val reset_global : unit -> unit
+end = struct
+  open Lexing
+
+  type cell =
+    | Empty
+    | Node of {
+        pos: position;
+        mutable value: elt;
+        mutable prev: cell;
+        mutable next: cell;
+      }
+
+  module Tbl =
+    Hashtbl.Make(struct
+      type t = position
+      let equal t1 t2 = Int.equal t1.pos_cnum t2.pos_cnum
+      let hash t = Hashtbl.hash t.pos_cnum
+    end)
+
+  type t = {
+    tbl: cell Tbl.t;
+    mutable last: cell;
+  }
+  let create () : t = { tbl = Tbl.create 42; last = Empty }
+
+  let append t ~pos desc =
+    let elt = { desc; pos } in
+    let node = Node { pos; value = elt; prev = t.last; next = Empty } in
+    Tbl.add t.tbl pos node;
+    begin match t.last with
+    | Empty -> ()
+    | Node n -> n.next <- node
+    end;
+    t.last <- node
+
+  let insert_child t pos =
+    dprintf "Inserting Child_node for empty reduction at pos %d:%d@\n"
+      pos.pos_lnum (pos.pos_cnum - pos.pos_bol);
+    let[@warning "-8"] (Node node) as cell =
+      let value = { desc = Child_node; pos } in
+      Node { pos; value; prev = Empty; next = Empty }
+    in
+    Tbl.add t.tbl pos cell;
+    match t.last with
+    | Empty -> t.last <- cell
+    | Node _ as last ->
+      let rec aux = function
+        | Empty -> assert false
+        | Node n as n_cell ->
+          if n.pos < pos then (
+            node.next <- n.next;
+            begin match n.next with
+            | Empty -> ()
+            | Node next ->
+              dprintf "prev = %a@ next = %a@."
+                pp_elt n.value pp_elt next.value;
+              next.prev <- cell
+            end;
+            n.next <- cell;
+            node.prev <- n_cell
+          ) else (
+            match n.prev with
+            | Node _ -> aux n.prev
+            | Empty ->
+              node.next <- n_cell;
+              n.prev <- cell
+          )
+      in
+      aux last
+
+  let consume t start stop =
+    dprintf "consume %d:%d -> %d:%d@."
+      start.pos_lnum (start.pos_cnum - start.pos_bol)
+      stop.pos_lnum (stop.pos_cnum - stop.pos_bol);
+    let rec aux = function
+      | Empty -> invalid_arg "Tokens.consume"
+      | Node n as curr ->
+        if n.pos >= stop then (
+          (* Stop is an endpos, and we index by startpos *)
+          [], curr
+        ) else (
+          Tbl.remove t.tbl n.pos;
+          let seq, after = aux n.next in
+          n.value :: seq, after
+        )
+    in
+    match Tbl.find t.tbl start with
+    | exception Not_found ->
+      (* Nothing registered: we are reducing an empty rule *)
+      assert (start = stop);
+      (* We will return an empty list of tokens, but we must remember to insert
+         a [Child_node] entry at that position, because the parent node in the
+         token tree will expect a child, even if it's empty. *)
+      insert_child t start;
+      []
+    | Empty -> invalid_arg "Tokens.consume"
+    | Node start_node as cell ->
+      let seq, after = aux cell in
+      start_node.value <- { start_node.value with desc = Child_node };
+      start_node.next <- after;
+      begin match after with
+      | Empty -> ()
+      | Node n -> n.prev <- cell
+      end;
+      Tbl.replace t.tbl start cell; (* has been removed by aux... *)
+      Tbl.replace t.tbl stop cell;
+      seq
+
+  let consume t start stop =
+    let res = consume t start stop in
+    dprintf "consumed: @[<h 2>%a@]@." pp_seq res;
+    res
+
+  let global = create ()
+  let reset_global () =
+    Tbl.clear global.tbl;
+    global.last <- Empty
+end
+
+let at (startpos,endpos) =
+  Indexed_list.(consume global startpos endpos)
+
