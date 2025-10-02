681b32044944a747e9d79850095ffb4a9710f44c
Thomas Refis <thomas.refis@gmail.com>
2025-10-02 14:29:35 +0200

    comments attachment: enable new approach in mlis

--- parsing/parse.ml
+++ parsing/parse.ml
@@ -1,13 +1,3 @@
-let lex_and_save lexbuf =
-  let open Tokens.Indexed_list in
-  let next_tok = Lexer.token lexbuf in
-  Queue.iter (fun (c, loc) ->
-    append global ~pos:loc.Location.loc_start (Comment (c, Before))
-  ) Lexer.comments;
-  Queue.clear Lexer.comments;
-  append global ~pos:lexbuf.lex_start_p (Token next_tok);
-  next_tok
-
 let implementation lb =
   Lexer.init ();
   Tokens.Indexed_list.reset_global ();
@@ -19,7 +9,7 @@ let implementation lb =
 let interface lb =
   Lexer.init ();
   Tokens.Indexed_list.reset_global ();
-  let sg = Parser.interface lex_and_save lb in
+  let sg = Parser.interface Lexer.token_updating_indexed_list lb in
   let all_tokens = Tokens.Indexed_list.(consume_all global) in
   { sg with
     psg_tokens = Tokens.replace_first_child ~subst:sg.psg_tokens all_tokens }
