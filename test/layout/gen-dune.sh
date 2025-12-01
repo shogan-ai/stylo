b136f399a26c5ebb24645ad2013e0f12220ed5e4
Thomas Refis <thomas.refis@gmail.com>
2025-12-01 13:33:00 +0100

    test: add layout/failing, and accept some of the current outputs
    Keep the expected (i.e. oxcamlformat's) output, so we can compare when
    the output changes.

--- test/layout/gen-dune.sh
+++ test/layout/gen-dune.sh
@@ -1,6 +1,17 @@
 function check-file {
     local f="$1"
     local name=$(basename $(basename $f ".mli") ".ml")
+
+    # In failing/ we always have a .oxcamlformat-ref file, to record what
+    # oxcamlformat does.
+    # But sometimes we also have a .ref file: this indicates that we deem
+    # acceptable to have a different output from oxcamlformat.
+    if [ -f "$name.ref" ]; then
+        ref="$name.ref"
+    else
+        ref="$name.oxcamlformat-ref"
+    fi
+
     cat <<EOF
 (rule
   (target $name.out)
@@ -16,7 +27,7 @@ function check-file {
 
 (rule
   (alias runtest)
-  (action (diff %{dep:$name.ref} %{dep:$name.out})))
+  (action (diff %{dep:$ref} %{dep:$name.out})))
 
 ; idempotence check
 (rule
