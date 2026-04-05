792a2921e405f63eef362d3042dbe0095aa9b2a3
Frédéric Bour <frederic.bour@lakaban.net>
2026-04-06 08:12:12 +0900

    WIP: Custom syntax for inlining

--- tools/print-grammar/print_grammar.ml
+++ tools/print-grammar/print_grammar.ml
@@ -1,3 +1,5 @@
+let inlined_terms = ref []
+
 let () = if Array.length Sys.argv <> 2 || not (Sys.file_exists Sys.argv.(1)) then (
     if Array.length Sys.argv = 2 then
       Printf.eprintf "File %S does not exist.\n\n" Sys.argv.(1);
@@ -13,6 +15,9 @@ module G = MenhirSdk.Cmly_read.Read(struct
   end)
 
 open G.Surface
+
+let errorf fmt =
+  Printf.kfprintf (fun _ -> exit 1) stderr fmt
 
 (* Reverse engineer anonymous producers.
    Minor bug in Menhir: the grammar is serialized post lambda-lifting. It shouldn't. *)
@@ -53,6 +58,93 @@ let resolve_anonymous param =
         Anonymous (Rule.branches rule)
     end
   | Anonymous _ -> desc
+
+(* Lift to an internal syntax for custom inlining *)
+
+type term =
+  | App of string * term list
+  | Var of string
+  | Anonymous of branch list
+
+and branch = term list
+
+let rec lift_parameter param =
+  match resolve_anonymous param with
+  | App (name, args) ->
+    App (name, List.map lift_parameter args)
+  | Var name -> Var name
+  | Anonymous branches ->
+    Anonymous (List.map lift_branch branches)
+
+and lift_producer prod = lift_parameter (Producer.symbol prod)
+
+and lift_branch branch = List.map lift_producer (Branch.producers branch)
+
+let lift_rule rule =
+  (Rule.parameters rule, List.map lift_branch (Rule.branches rule))
+
+let rules =
+  let table = Hashtbl.create 7 in
+  List.iter (fun (name, def) ->
+      Hashtbl.add table name (lift_rule def)
+    ) (Syntax.rules before_expansion);
+  table
+
+(* Inline matching terms *)
+
+let need_inlining =
+  let table = Hashtbl.create 7 in
+  let is_operator = function
+    | ',' | '(' | ')' -> true
+    | _ -> false
+  in
+  let parse_term term =
+    if String.exists (function
+        | ' ' -> errorf "inlining term: ' ' not allowed in %S\n" term
+        | c -> is_operator c
+      ) term
+    then
+      let position = ref 0 in
+      let tokens = ref [] in
+      let flush pos =
+        if !position <> pos then
+          tokens := String.sub term !position (pos - !position) :: !tokens;
+        position := pos + 1
+      in
+      for i = 0 to String.length term - 1 do
+        if is_operator term.[i] then (
+          flush i;
+          tokens := String.make 1 term.[i] :: !tokens
+        )
+      done;
+      let malformed () = errorf "inlining term: %S is malformed\n" term in
+      if !position <> String.length term then
+        malformed ();
+      let rec parse_param = function
+        | ("," | "(" | ")") :: _ -> malformed ()
+        | name :: "(" :: rest ->
+          let params, rest = parse_params rest in
+          (App (name, params), rest)
+        | name :: rest ->
+          (Var name, rest)
+        | [] -> malformed ()
+      and parse_params = function
+        | "," :: rest ->
+          let param, rest = parse_param rest in
+          let params, rest = parse_params rest in
+          (param :: params, rest)
+        | ")" :: rest ->
+          ([], rest)
+        | _ :: _ -> malformed ()
+        | [] -> malformed ()
+      in
+      match parse_param (List.rev !tokens) with
+      | term, [] -> term
+      | _, _ -> malformed ()
+    else Var term
+  in
+  List.iter (fun term -> Hashtbl.add table (parse_term term) ()) !inlined_terms;
+  fun term -> Hashtbl.mem table term
 
 (* Print *)
 
