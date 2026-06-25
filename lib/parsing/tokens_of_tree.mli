bd253cbd26aaa0f306268002a4ae8fd07f3c6f59
Thomas Refis <thomas@parce-q.eu>
2026-06-25 15:52:08 +0200

    noise

--- lib/parsing/tokens_of_tree.mli
+++ lib/parsing/tokens_of_tree.mli
@@ -22,8 +22,11 @@ open Parsetree
 val structure : structure -> (Tokens.seq, [> Error.t ]) result
 val signature : signature -> (Tokens.seq, [> Error.t ]) result
 
+(**/**)
+
 (* Used during normalization when subtrees are erased but we want to keep the
-   comments inside them. *)
+   comments inside them: we retrieve the tokens of the deleted subtree, and keep
+   only the comments (which we'll attach to the new node, or parent). *)
 
 val modes : modes -> (Tokens.seq, [> Error.t ]) result
 val jkind_annotation : jkind_annotation -> (Tokens.seq, [> Error.t ]) result
