aeeaabf05d11897cdb9d39ca74f830033ac34458
Thomas Refis <thomas@parce-q.eu>
2026-09-01 09:12:36 +0200

    dune fmt calls stylo

--- tools/dune-stylo-wrapper.sh
+++ tools/dune-stylo-wrapper.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
 case $1 in
     "--impl"|"--intf")
         shift 1
@@ -10,4 +12,19 @@ case $1 in
         exit 1
 esac
 
-stylo style --idempotence-check --ast-check --width 80 $1
+if [[ -f "$ROOT/stylo.opam" ]]; then
+    # we are running locally in the workspace, so don't look for stylo in path
+    STYLO="$ROOT/bin/main.exe"
+
+    # also, leave vendor/ and test/ alone
+    case $1 in
+        vendor/*|test/*)
+            cat $1 # reemit as is
+            exit 0
+            ;;
+    esac
+else
+    STYLO=stylo
+fi
+
+$STYLO style --idempotence-check --ast-check --width 80 "$1"
