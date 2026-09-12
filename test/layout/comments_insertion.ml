2f058982c3b9cac1a74202a5ccd72d0c0700eb6b
Thomas Refis <thomas@shogan.ai>
2026-09-12 20:07:53 +0900

    test: highlight floating comments poor rendering

--- test/layout/comments_insertion.ml
+++ test/layout/comments_insertion.ml
@@ -13,6 +13,15 @@ let foo =
 
 (* floating2 *)
 
+(* a series of floating comments close to each other *)
+(* here is the second, not particularly interesting, one *)
+(* the last one even has
+   {[
+     a code block
+   ]} *)
+
+(* floating3 *)
+
 let foo2 =
   if whatever2
   then
@@ -22,3 +31,20 @@ let foo2 =
     match not_flat2 with
     | () -> ()
   )
+
+(** floating comments between match cases perhaps? *)
+let foo = function
+  (* not floating, but natural placing *)
+  | Some pattern
+  | Or (Another pattern) -> ignore pattern
+
+  (* now, that one floats, to inform us that the next one's won't float but be
+     attached. *)
+
+  (* right? *)
+  | Yes right -> right
+
+  (* same as well, except longer because we want to test that too *)
+  | I_am talking -> myself
+
+(* ... *)
